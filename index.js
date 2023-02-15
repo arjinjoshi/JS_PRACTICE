064878aedd734498ddb1df9c8744279f67a7c261
Arjin Joshi <thearjinjoshi1@gmail.com>
2023-02-16 03:06:13 +0545

    Day 1[COMPLETED]

---
+++ index.js
@@ -0,0 +1,95 @@
+//line is used to print on the console
+// console.log('hello guys v3');
+// let a = 5;
+
+// let b = 'arjin';
+// let c = 5.2;
+// // c = true;
+// let e = 17;
+// // e=17;
+// console.log(a);
+// // a = 'joshi';
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(e);
+// let f = e / a;
+// let g = a ** c;
+
+// console.log(f);
+// console.log(g);
+// console.log(2 > 3);
+// console.log(2 === 3);
+
+
+// ternary operator
+// let age = 17;
+// let status = (age >= 18) ? 'yes vote' : 'not vote';
+// console.log(status);
+
+// logical operator with non boolean 
+// console.log(false && 'arjin');
+// console.log(true && 'arjin');
+// console.log(true && 0);
+// console.log(false && 12);
+// console.log('arjin' && 'joshi');
+// console.log('arjin' && 'joshi');
+// console.log('arjin' && 'joshi' && 'hello');
+// console.log(1 && 5 && 16);
+
+
+// if else ef else case 
+// let marks = 98;
+// if (marks >= 90) {
+//     console.log('A');
+// }
+// else if (marks >= 80) {
+//     console.log('B');
+// }
+// else if (marks >= 70) {
+//     console.log('C');
+// }
+// else if (marks >= 60) {
+//     console.log('D');
+// }
+// else {
+//     console.log('E');
+// }
+
+
+// switch case 
+// let num = 2;
+// switch (num) {
+//     case 1: console.log('A');
+//         break;
+//     case 2: console.log('B');
+//         break;
+//     case 3: console.log('C');
+//         break;
+//     case 4: console.log('D');
+//         break;
+//     default: console.log('E');
+// }
+
+// for loop
+// for (let i = 0; i <= 5; i += 1) {
+//     console.log(i);
+// }
+
+// while loop
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+// let y = 1;
+// do {
+//     console.log(y);
+//     y++;
+// } while (y <= 10);
+
+
+
+
